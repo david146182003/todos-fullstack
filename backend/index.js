628ee0a47c70058f3d92930a013552830ef9a3f6
david <david146182003@gmail.com>
2025-07-14 15:03:52 -0400

    first commit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,9 @@ const app = express()
 
 const port = process.env.PORT
 
+app.use(express.json())
 app.use(cors())
+
 
 app.get('/todos', async (req, res)=>{
 
@@ -21,6 +23,44 @@ app.get('/todos', async (req, res)=>{
         res.status(400).json(e)
     }
     
+})
+
+app.post('/todos', async(req, res)=>{
+
+    try{
+        const todo = await Todo.create(req.body)
+        res.status(200).json(todo)
+        // const todo = new Todo(req.body)
+        // await todo.save()
+    }catch(e){
+        res.status(400).json(e)
+    }
+})
+
+app.delete('/todos/:id', async (req, res)=>{
+    try{
+        console.log('Delete /todos/:id')
+        const response = await Todo.findByIdAndDelete(req.params.id)
+        console.log(response)
+        res.status(200).json(response)
+    }catch(e){
+        console.log(e)
+        res.status(400).json(e)
+    }
+})
+
+app.put('/todos/:id', async(req, res)=>{
+    try{
+        console.log('PU / todod/:id')
+        const todo = await Todo.findById(req.params.id)
+        todo.completed = !todo.completed
+        await todo.save()
+        console.log(todo)
+        res.json(todo)
+    }catch(e){
+        console.log(e)
+        res.json(e)
+    }
 })
 
 app.listen(port, ()=>{
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,93 @@
 
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 
+const BASE_URL = 'http://localhost:8080'
+
+
+
 function App() {
+
+  const [todos, setTodos] = useState([])
+  const inputRef = useRef()
   
-  useEffect(() => {
-    async function getData(){
+  async function getData(){
       try{
-        const response = await fetch('http://localhost:8080')
+        const response = await fetch(BASE_URL + '/todos')
         const data = await response.json()
-        console.log(data)
+
+        setTodos(data)
+
       }catch(e){
         console.log(e)
       }
     }
+
+  useEffect(() => {
+    
     getData()
 
   }, [])
+  console.log(todos)
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    const todo = {
+      text: inputRef.current.value
+    }
+    // send this todo to our server
+    const response = await fetch(BASE_URL + '/todos',{
+      method: 'POST',
+      body: JSON.stringify(todo),
+      headers:{
+        'Content-Type': "application/json"
+      }
+    })
+    const newTodo = await response.json()
+    console.log(newTodo)
+    setTodos([...todos, newTodo])
+
+    inputRef.current.value =''
+  }
+
+  async function handleDelete(id){
+    await fetch(BASE_URL + '/todos/' + id, {
+      method: 'DELETE'
+    })
+    getData()
+  }
+
+  async function handleCheck(id){
+     await fetch(BASE_URL + '/todos/' + id, {
+      method: 'PUT',
+      
+    })
+    getData()
+    
+
+  }
 
   return (
     <>
-      Hello (from frontend)
+      <h1>Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef}  />
+        <button>Submit</button>
+      </form>
+      <ul>
+        {todos.map((todo) =>
+          <li key ={todo._id}>
+            <input 
+              type="checkbox" 
+              checked={todo.completed}
+              onChange={()=>handleCheck(todo._id)}
+            />
+            {todo.text}
+            <button onClick={() => handleDelete(todo._id)}>X</button>
+            </li>
+        )}
+      </ul>
+     
     </>
   )
 }
